4b4ae8df729736cc157fac3ae40be75967ce230a
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-11 22:01:22 -0500

    Commit Build
    So others can interact with this a bit faster.

---
+++ build/admin/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f3838a743be141865143');
---
+++ build/editor/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'b8b0f63a1b3b1be5d3fb');
