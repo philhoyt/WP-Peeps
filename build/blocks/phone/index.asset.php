f21c9b8b363d510fc350ea49ee6de979a32a24f2
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-12 14:08:45 -0500

    Clean Up

--- build/blocks/phone/index.asset.php
+++ build/blocks/phone/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '0734c593b4c3c0b7585e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => 'e48461c1f97bc011fe76');
