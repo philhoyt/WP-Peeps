aacc944bfe2bda0f9f2541ce657e0b3dd294231a
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-13 17:09:06 -0500

    Build

---
+++ build/admin/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '7075b37113bd223f0d6b');
