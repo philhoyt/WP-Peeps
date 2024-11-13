164f3a8db8ede1e1b3bace35d26fa44228679385
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-13 17:07:41 -0500

    DO IT LIVE

--- build/admin/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '37f07b5f138e78cf6177');
--- build/editor/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '8ac6e99194e41dd181e7');
