6bb8f56cceda359e3368e276e590394e4da003c0
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-14 07:21:37 -0500

    Revert "Add a return to default link for slug"
    This reverts commit 456afeb774dd774522b9630e7916bfe4bf0ad99f.

--- build/blocks/phone/index.asset.php
+++ build/blocks/phone/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '2a9b78caaf4ef91cdf76');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '355f7623e38f4ecf80dc');
