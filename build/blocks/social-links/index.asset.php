485508f5942b0b24b82dcadc60ceb654b0f08b07
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-14 20:34:36 -0500

    Remove build directory from git tracking

--- build/blocks/social-links/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '9bd7ed9e4e4278876f4c');
--- build/editor/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-server-side-render'), 'version' => 'b944a69d075cfd217b09');
