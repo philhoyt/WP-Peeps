c5368cdcbee575cfd660a12898ecb8d2a110062a
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-15 19:09:40 -0500

    Remove save files on dynamic blocks

--- src/blocks/full-name/index.js
+++ src/blocks/full-name/index.js
@@ -1,17 +1,8 @@
 import { registerBlockType } from '@wordpress/blocks';
 
 import Edit from './edit';
-import Save from './save';
 import metadata from './block.json';
 
 registerBlockType( metadata.name, {
-	/**
-	 * @see ./edit.js
-	 */
 	edit: Edit,
-
-	/**
-	 * @see ./save.js
-	 */
-	save: Save,
 } );
--- src/blocks/phone/save.js
+++
@@ -1,5 +0,0 @@
-import { useBlockProps } from '@wordpress/block-editor';
-
-export default function Save() {
-	return null; // Dynamic block, rendered in PHP
-}
