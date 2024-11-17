49e102ee702ba467fe42e56755dab9e6522cc431
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-16 21:10:34 -0500

    Revert "Setup WordPress Playground"
    This reverts commit 7c00ee1127e44766d931aa8966bc46886e94b5d6.

--- build/blocks/full-name/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n', 'wp-primitives'), 'version' => '89af2d0e4109cc5284bc');
