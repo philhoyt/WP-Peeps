7c00ee1127e44766d931aa8966bc46886e94b5d6
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-16 21:03:47 -0500

    Setup WordPress Playground

---
+++ build/blocks/phone/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '88c7db8261899320e17b');
