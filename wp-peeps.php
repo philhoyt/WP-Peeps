3f8cf44065f14284fc8f04a9a976cd51dfaef35a
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-19 12:59:03 -0500

    Update wp-peeps.php
    Add details

--- wp-peeps.php
+++ wp-peeps.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * Plugin Name:       WP Peeps
+ * Plugin URI:        https://github.com/philhoyt/WP-Peeps
  * Description:       A directory of people for your WordPress site.
  * Requires at least: 6.6
  * Requires PHP:      7.2
  * Version:           0.1.0
  * Author:            Phil Hoyt
+ * Author URI:        https://philhoyt.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wp-peeps
