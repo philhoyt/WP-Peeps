27c38b197109e1cf7d4ec96e03a3532e0c349faf
Phil Hoyt <philhoyt@users.noreply.github.com>
2026-02-24 12:59:10 -0500

    build

--- build/blocks/social-links/index.asset.php
+++ build/blocks/social-links/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '09da92513ea0ecead665');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'a41260babf2e299ecb69');
