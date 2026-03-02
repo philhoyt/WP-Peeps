f34ed533ea4e009ac78175e42b114b79b2fcc7c2
Phil Hoyt <philhoyt@users.noreply.github.com>
2026-03-02 15:27:32 -0500

    Focus on Name field

--- build/editor/index.asset.php
+++ build/editor/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-server-side-render'), 'version' => '5452d87a88735c0f8339');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-server-side-render'), 'version' => '93c10275ed2e2e1bb8de');
